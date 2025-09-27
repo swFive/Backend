5f11e33b0c4cf4c70fa3c172a6ddd3b30f7d5946
SujinAhn21 <theroseey23@naver.com>
2025-09-27 18:51:50 +0900

    초기 설정

---
+++ src/test/java/com/example/medicineReminder/MedicineReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicineReminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
