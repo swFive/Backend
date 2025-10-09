956fcb1b60673a447473a4fffb09c121e016baee
minju <sfhketum1231@gmail.com>
2025-10-09 21:58:52 +0900

    feat: 스웨거 UI 접근 허용 및 OAuth2 로그인 DB 연동
    인증 없이 스웨거 UI에 접근할 수 있도록 보안 설정을 변경했습니다.
    
    OAuth2 로그인 시 CustomOAuth2UserService가 동작하도록 연결하여, 신규 사용자 정보가 DB에 저장되도록 기능을 활성화했습니다.

--- src/main/java/com/example/medicineReminder/config/SecurityConfig.java
+++ src/main/java/com/example/medicineReminder/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.medicineReminder.config;
 
 
+import com.example.medicineReminder.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,15 +13,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        // Swagger UI 경로 접근 허용
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
                         .requestMatchers("/", "/login").permitAll() // 루트 경로와 로그인 경로는 모두 허용
                         .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 )
-                .oauth2Login(withDefaults()); // OAuth 2.0 로그인 활성화
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                // 로그인 성공 후 처리는 우리가 만든 customOAuth2UserService를 사용하라고 지정
+                                .userService(customOAuth2UserService)
+                        )
+                );
 
         return http.build();
     }
